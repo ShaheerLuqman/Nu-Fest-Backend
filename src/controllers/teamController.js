0b82651d2915dc9fde80574ccd6ae99b678bf871
R64 <ammar.qaboolio7704@gmail.com>
2025-01-21 17:39:05 +0500

    .

--- src/controllers/teamController.js
+++ src/controllers/teamController.js
@@ -19,6 +19,165 @@ import pool from "../db/connection.js";
 //307 means leader already exists
 //
 
+// export const addTeam = async (req, res) => {
+//   const addTeamQuery = `
+//     INSERT INTO team (team_name, competition_id, payment_screenshot, team_leader_id, team_leader_phone)
+//     VALUES ($1, $2, $3, NULL, $4)
+//     RETURNING team_id;
+//   `;
+//   const addParticipantQuery = `
+//     INSERT INTO participant (part_name, email, competition_id, team_id)
+//     VALUES ($1, $2, $3, $4)
+//     RETURNING part_id;
+//   `;
+//   const updateTeamLeaderQuery = `
+//     UPDATE team
+//     SET team_leader_id = $1
+//     WHERE team_id = $2;
+//   `;
+//   const checkTeamQuery = `
+//     SELECT *
+//     FROM team
+//     WHERE team_name = $1 AND competition_id = $2;
+//   `;
+//   const checkParticipantQuery = `
+//     SELECT *
+//     FROM participant
+//     WHERE email = ANY($1::text[]) AND competition_id = $2;
+//   `;
+
+//   const teamLeaderExistsQuery = `
+//     SELECT part_id
+//     FROM participant
+//     WHERE email = $1 AND competition_id = $2;
+//   `;
+
+//   try {
+//     const {
+//       team_name,
+//       competition_id,
+//       payment_screenshot,
+//       team_leader,
+//       participants,
+//     } = req.body;
+
+//     // Validate input
+//     if (
+//       !team_leader ||
+//       !team_leader.name ||
+//       !team_leader.email ||
+//       !team_leader.phone
+//     ) {
+//       return res
+//         .status(400)
+//         .json({ message: "Team leader information is required" });
+//     }
+//     // const getCompetitionTeamsQuery = `
+//     //   select maxplayersperteam, minplayersperteam from nufest.competitions where id = $1;
+//     // `;
+//     // const competitionTeams = await pool.query(getCompetitionTeamsQuery, [
+//     //   competition_id,
+//     // ]);
+//     // if (
+//     //   !(
+//     //     participants &&
+//     //     participants.length >= competitionTeams.rows[0].minplayersperteam - 1
+//     //   )
+//     // ) {
+//     //   res.status(306).json({
+//     //     message: `${competitionTeams.rows[0].minplayersperteam} to ${competitionTeams.rows[0].maxplayersperteam} participants are required`,
+//     //   });
+//     // }
+//     // Check if the team already exists and participants as well
+//     const teamExists = await pool.query(checkTeamQuery, [
+//       team_name,
+//       competition_id,
+//     ]);
+//     if (teamExists.rows.length > 0) {
+//       return res.status(305).json({ message: "Team already exists" });
+//     }
+//     //Check if the participants already exists
+//     if (participants && participants.length > 0) {
+//       const participantEmails = participants.map((p) => p.email);
+//       const existingParticipants = await pool.query(checkParticipantQuery, [
+//         participantEmails,
+//         competition_id,
+//       ]);
+
+//       if (existingParticipants.rows.length > 0) {
+//         return res.status(306).json({
+//           message: "Some participants are already registered",
+//           existingParticipants: existingParticipants.rows.map((p) => ({
+//             email: p.email,
+//           })),
+//         });
+//       }
+//     }
+//     const existingTeamLeader = await pool.query(teamLeaderExistsQuery, [
+//       team_leader.email.toLowerCase(),
+//       competition_id,
+//     ]);
+
+//     if (existingTeamLeader.rows.length > 0) {
+//       // Use the existing participant ID
+//       return res.status(307).json({
+//         message: "Team Leader is already registered",
+//       });
+//     }
+//     const teamResult = await pool.query(addTeamQuery, [
+//       team_name,
+//       competition_id,
+//       payment_screenshot,
+//       team_leader.phone,
+//     ]);
+
+//     const teamId = teamResult.rows[0].team_id;
+//     const leaderResult = await pool.query(addParticipantQuery, [
+//       team_leader.name,
+//       team_leader.email,
+//       competition_id,
+//       teamId,
+//     ]);
+//     const teamLeaderId = leaderResult.rows[0].part_id;
+
+//     const updateTeamLeaderQuery = `
+//       UPDATE team
+//       set team_leader_id = $1
+//       where team_id = $2;
+//       `;
+//     await pool.query(updateTeamLeaderQuery, [teamLeaderId, teamId]);
+//     // Step 1: Insert the team (without leader ID initially)
+
+//     // Step 2: Add the team leader as a participant and retrieve their part_id
+
+//     console.log("participantEmails:", participants);
+//     console.log("competition_id:", competition_id);
+
+//     // Step 3: Update the team with the team leader ID
+//     await pool.query(updateTeamLeaderQuery, [teamLeaderId, teamId]);
+
+//     // Step 4: Add other participants
+//     if (participants && participants.length > 0) {
+//       for (const participant of participants) {
+//         const { name, email } = participant;
+//         await pool.query(addParticipantQuery, [
+//           name,
+//           email,
+//           competition_id,
+//           teamId,
+//         ]);
+//       }
+//     }
+
+//     res.status(201).json({ message: "Team added successfully" });
+//     console.clear();
+//   } catch (error) {
+//     // console.error("Error adding team:", error);
+//     res.status(500).json({ message: "Failed to add team" });
+//     console.clear();
+//   }
+// };
+
 export const addTeam = async (req, res) => {
   const addTeamQuery = `
     INSERT INTO team (team_name, competition_id, payment_screenshot, team_leader_id, team_leader_phone) 
@@ -72,23 +231,7 @@ export const addTeam = async (req, res) => {
         .status(400)
         .json({ message: "Team leader information is required" });
     }
-    // const getCompetitionTeamsQuery = `
-    //   select maxplayersperteam, minplayersperteam from nufest.competitions where id = $1;
-    // `;
-    // const competitionTeams = await pool.query(getCompetitionTeamsQuery, [
-    //   competition_id,
-    // ]);
-    // if (
-    //   !(
-    //     participants &&
-    //     participants.length >= competitionTeams.rows[0].minplayersperteam - 1
-    //   )
-    // ) {
-    //   res.status(306).json({
-    //     message: `${competitionTeams.rows[0].minplayersperteam} to ${competitionTeams.rows[0].maxplayersperteam} participants are required`,
-    //   });
-    // }
-    // Check if the team already exists and participants as well
+
     const teamExists = await pool.query(checkTeamQuery, [
       team_name,
       competition_id,
@@ -96,7 +239,7 @@ export const addTeam = async (req, res) => {
     if (teamExists.rows.length > 0) {
       return res.status(305).json({ message: "Team already exists" });
     }
-    //Check if the participants already exists
+
     if (participants && participants.length > 0) {
       const participantEmails = participants.map((p) => p.email);
       const existingParticipants = await pool.query(checkParticipantQuery, [
@@ -113,25 +256,25 @@ export const addTeam = async (req, res) => {
         });
       }
     }
+
     const existingTeamLeader = await pool.query(teamLeaderExistsQuery, [
       team_leader.email.toLowerCase(),
       competition_id,
     ]);
-
     if (existingTeamLeader.rows.length > 0) {
-      // Use the existing participant ID
-      return res.status(307).json({
-        message: "Team Leader is already registered",
-      });
+      return res
+        .status(307)
+        .json({ message: "Team Leader is already registered" });
     }
+
     const teamResult = await pool.query(addTeamQuery, [
       team_name,
       competition_id,
       payment_screenshot,
       team_leader.phone,
     ]);
-
     const teamId = teamResult.rows[0].team_id;
+
     const leaderResult = await pool.query(addParticipantQuery, [
       team_leader.name,
       team_leader.email,
@@ -140,23 +283,8 @@ export const addTeam = async (req, res) => {
     ]);
     const teamLeaderId = leaderResult.rows[0].part_id;
 
-    const updateTeamLeaderQuery = `
-      UPDATE team 
-      set team_leader_id = $1
-      where team_id = $2;
-      `;
-    await pool.query(updateTeamLeaderQuery, [teamLeaderId, teamId]);
-    // Step 1: Insert the team (without leader ID initially)
-
-    // Step 2: Add the team leader as a participant and retrieve their part_id
-
-    console.log("participantEmails:", participants);
-    console.log("competition_id:", competition_id);
-
-    // Step 3: Update the team with the team leader ID
     await pool.query(updateTeamLeaderQuery, [teamLeaderId, teamId]);
 
-    // Step 4: Add other participants
     if (participants && participants.length > 0) {
       for (const participant of participants) {
         const { name, email } = participant;
@@ -168,10 +296,15 @@ export const addTeam = async (req, res) => {
         ]);
       }
     }
+    console.clear(); // Clears the console
 
     res.status(201).json({ message: "Team added successfully" });
+    console.clear(); // Clears the console
   } catch (error) {
-    // console.error("Error adding team:", error);
+    console.error("Error adding team:"); // Log the error
+    console.clear(); // Clears the console
+
     res.status(500).json({ message: "Failed to add team" });
+    console.clear(); // Clears the console
   }
 };
